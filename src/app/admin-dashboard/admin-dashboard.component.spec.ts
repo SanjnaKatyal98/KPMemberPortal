1250e0fc276d2b83fe8971e7d52a11d4d2bec2eb
Sanjna Katyal <73328935+SanjnaKatyal98@users.noreply.github.com>
2023-05-25 13:35:24 +0530

    Add files via upload

---
+++ src/app/admin-dashboard/admin-dashboard.component.spec.ts
@@ -0,0 +1,28 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AdminDashboardComponent } from './admin-dashboard.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('AdminDashboardComponent', () => {
+  let component: AdminDashboardComponent;
+  let fixture: ComponentFixture<AdminDashboardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdminDashboardComponent ],
+      imports: [
+        RouterTestingModule,HttpClientTestingModule,FormsModule,ReactiveFormsModule
+      ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AdminDashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
