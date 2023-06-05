5378538bda35cfb7aba2f6834dad401d3786b8f4
Sanjna Katyal <73328935+SanjnaKatyal98@users.noreply.github.com>
2023-06-05 22:00:37 +0530

    Delete spring/registerNlogin/src directory

--- spring/registerNlogin/src/main/java/com/example/demo/repos/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.models.Role;
-
-public interface RoleRepository extends JpaRepository<Role,Integer>{
-	//role se related DB operations honge
-	//DB mei cheeze save hongi
-}
